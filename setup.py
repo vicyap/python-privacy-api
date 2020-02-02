6c2791f21250a523b533d2fdc086da3d7b52a756
Victor Yap <github@vicyap.com>
2020-02-02 14:56:36 -0600

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ DESCRIPTION = "API client for privacy.com"
 URL = "https://github.com/vicyap/python-privacy-api"
 AUTHOR = "Victor Yap"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["requests"]
