7899a4169787aeac68ac85b4c91a22dae6290115
Victor Yap <github@vicyap.com>
2020-02-03 00:51:09 -0600

    Add example transaction for testing schemas

--- tests/test_models.py
+++
@@ -1,5 +0,0 @@
-from privacy_api import models  # pylint: disable=unused-import
-
-
-def test_models():
-    pass
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,5 +1,18 @@
-from privacy_api import schemas  # pylint: disable=unused-import
+import json
+import os
+
+from privacy_api import schemas
 
 
-def test_schemas():
-    pass
+def test_transaction_schema():
+    test_data_dir = os.path.join(os.path.dirname(__file__), "data")
+    with open(os.path.join(test_data_dir, "example_transaction.json")) as f:
+        json_data = json.loads(f.read())
+    t = schemas.TransactionSchema().load(json_data)
+    expected_token = "00000000-0000-0000-0000-000000000000"
+    assert t.token == expected_token
+    assert t.funding[0].token == expected_token
+    assert t.events[0].token == expected_token
+    assert t.card.funding.token == expected_token
+    assert t.card.token == expected_token
+    assert t.merchant.descriptor == "merchant"
